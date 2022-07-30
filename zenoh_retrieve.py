9912d3bde0c942665f2bde26cb14655727b5591a
ottlukas <ott.lukas.14@gmail.com>
2022-07-30 13:14:36 +0200

    Retrieve Timestamp

--- zenoh_retrieve.py
+++ zenoh_retrieve.py
@@ -4,27 +4,9 @@ if __name__ == "__main__":
     z = Zenoh({'peer': 'tcp/127.0.0.1:7447'})
     w = z.workspace('/')
     results = w.get('/myfactory/machine1/temp')
-    key, value = results[0].path, results[0].value
-    print('  {} : {}'.format(key, value))
-    # Insert into IoTDB via Python IoTDB API
+    print (results)
+    key, value, timestamp = results[0].path, results[0].value, results[0].timestamp
+    print('  {} : {}'.format(key, value, timestamp))
+    print(type(timestamp))
+    # TODO Insert into IoTDB via Python IoTDB API
     #...
-"""
-from iotdb.Session import Session
-
-ip = "127.0.0.1"
-port_ = "6667"
-username_ = "root"
-password_ = "root"
-session = Session(ip, port_, username_, password_)
-session.open(False)
-result = session.execute_query_statement("SELECT * FROM root.*")
-
-# Transform to Pandas Dataset
-df = result.todf()
-
-session.close()
-
-# Now you can work with the dataframe
-
-print(df)    
-"""
